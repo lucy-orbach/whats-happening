80cff387e9d2ffdb911304aae5c000b6e03e31a7
lucy-orbach <lucy.orbach@gmail.com>
2015-07-21 20:40:32 -0400

    added d3min file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 
 //= require noframework.waypoints.min.js
+//= require d3.min.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
